6acbe2349091668ee3288a213d7037697437873e
Jaroslaw <jerry.esp.sha@gmail.com>
2019-08-12 09:13:22 +0200

    Delete otodom-ftp.py

--- old-unused/otodom-ftp.py
+++
@@ -1,6 +0,0 @@
-import ftplib
-session = ftplib.FTP('y4r3k.boo.pl','y4r3k','ph@lenopsis.6578')
-file = open('otodom-zabawa.txt','rb')                  # file to send
-session.storbinary('STOR httpdocs/otodom-zabawa.txt', file)     # send the file
-file.close()                                    # close file and FTP
-session.quit()
